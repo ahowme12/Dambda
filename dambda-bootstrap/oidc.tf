67d0e15b6b3660eaa0da77797c352a44a07a76f3
fafe <ehdghks509797@gmail.com>
2026-08-05 12:44:33 +0900

    123

--- dambda-bootstrap/oidc.tf
+++ dambda-bootstrap/oidc.tf
@@ -31,8 +31,10 @@ resource "aws_iam_role" "github_actions_role" {
       }
       Condition = {
         StringLike = {
-          # push(main 반영)와 PR(plan-only) 이벤트의 sub 형식이 서로 달라서 둘 다 허용해야 함
-          "token.actions.githubusercontent.com:sub" = "repo:ahowme12/github-actions-test:*"
+            "token.actions.githubusercontent.com:sub" = [
+            "repo:ahowme12@80324068/github-actions-test@1308447274:ref:refs/heads/main",
+            "repo:ahowme12@80324068/github-actions-test@1308447274:pull_request"
+          ]
         }
       }
     }]
