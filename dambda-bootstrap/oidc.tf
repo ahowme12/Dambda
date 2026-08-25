aa1efe23149ce52489e65688bddd5e34be5e8355
fafe <ehdghks509797@gmail.com>
2026-08-25 10:26:37 +0900

    [skip ci]260825:v3

--- dambda-bootstrap/oidc.tf
+++ dambda-bootstrap/oidc.tf
@@ -45,8 +45,8 @@ resource "aws_iam_role" "github_actions_role" {
       Condition = {
         StringLike = {
           "token.actions.githubusercontent.com:sub" = [
-            "repo:ahowme12@80324068/dambda@1308447274:ref:refs/heads/main",
-            "repo:ahowme12@80324068/dambda@1308447274:pull_request"
+            "repo:ahowme12@80324068/Dambda@1308447274:ref:refs/heads/main",
+            "repo:ahowme12@80324068/Dambda@1308447274:pull_request"
           ]
         }
       }
